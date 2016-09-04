a9b1fea819466270143aca5f132c877711379471
tommaso <tommasoascari@gmail.com>
2016-09-04 23:12:49 +0200

    first file

---
+++ notify.py
@@ -0,0 +1,40 @@
+from gi.repository import Notify
+import os
+
+#  initialization of libnotify
+Notify.init("DOOMSDAY TRASH")
+
+#  notification object
+title = "Cleaning script is active"
+body = "Trash and download folders are going to be deleted, backup anything you need.\n you only have 5 minutes."
+
+notification = Notify.Notification.new(
+	title,
+	body,
+)
+
+#  function to call back
+
+def repl():
+	Notify.Notification.new(
+	"cleaning script delayed",
+	"in 20 minutes, all your trash is going to be erased forever.",
+	"/home/tommaso/Pictures/alert.png"
+	).show()
+	
+	return
+
+#add reply button
+
+notification.add_action(
+	"action_click",
+	"WAIT!",
+	repl(),
+	None #  Arguments
+)
+
+notification.set_urgency(2) #  highest priority notification
+
+
+notification.show()
+
