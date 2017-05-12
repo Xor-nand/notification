85673bf271b4a334d4caeb5b4458e5b403627042
tommasoascari <tommasoascari@gmail.com>
2017-05-12 22:36:19 +0200

    commit

--- lcd_test.py
+++ lcd_test.py
@@ -1,18 +1,39 @@
-import sys, time
-
+import sys, time, subprocess
 sys.path.append('/home/alarm/gpio')
-
 import lcd
+from subprocess import check_output, STDOUT, CalledProcessError
 
 lcd.lcd_init()
 
-true = True
+def myip():
+	try:
+		answer = check_output(["awk '{printf \"%3.1f°C\\n", $1/1000}' /sys/class/thermal/thermal_zone0/temp"], shell=True, stderr=STDOUT)
+		return answer
+	except CalledProcessError as exc :
+		return 1
 
-while true == True:
+def sdvolt(sdram):
+	pass
 
-	inin = input(">")
+def cpu():
+	pass
 
-	lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
-	lcd.lcd_string(inin, 2)
-	time.sleep(1)
+def loopmode():
+	cpu = cputemp()
+	sdram = sdvolt("sdram_c")
+	sdrami = sdvolt("sdram_i")
+	sdramp = sdvolt("sdram_p")
+	myip = ip()
+
+	print ip
+
+#ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
+
+#while true == True:
+
+	#inin = input(">")
+
+	#lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
+	#lcd.lcd_string(inin, 2)
+	#time.sleep(1)
 #	lcd.GPIO.cleanup()
