1272e2bc8c83eac3758aa8e1bbfc29d18a88487a
tommasoascari <tommasoascari@gmail.com>
2017-05-19 13:26:39 +0000

    lala

--- lcd_test.py
+++ lcd_test.py
@@ -1,39 +1,93 @@
-import sys, time, subprocess
-sys.path.append('/home/alarm/gpio')
+import sys, time, subprocess, re
+# using lcd library in the same syspath as this program
 import lcd
 from subprocess import check_output, STDOUT, CalledProcessError
 
+# simple logfile for headless running
+logger = open("lcsd.log", "w")
+# declaring all the lists we'll need
+ips = []
+blocks = []
+blocksip = []
+# initializing lcd
 lcd.lcd_init()
+time.sleep(10)
 
-def myip():
+def ifacecheck():		# return 'ip link' process output as 'wlp'
 	try:
-		answer = check_output(["awk '{printf \"%3.1f°C\\n", $1/1000}' /sys/class/thermal/thermal_zone0/temp"], shell=True, stderr=STDOUT)
-		return answer
+		wlp = check_output(["ip addr"], shell=True, stderr=STDOUT)
+		temp = check_output(["/opt/vc/bin/vcgencmd measure_temp"], shell=True, stderr=STDOUT)
+		volt = check_output(["/opt/vc/bin/vcgencmd measure_volts core"], shell=True, stderr=STDOUT)
+		voltio = check_output(["/opt/vc/bin/vcgencmd measure_volts "], shell=True, stderr=STDOUT)
+
+		logger.write("\n\nWLP:\n{}".format(wlp))
+		logger.write("\n\nTEMP:\n{}".format(temp))
+		logger.write("\n\nVOLT:\n{}".format(volt))
+		logger.flush()
+		return wlp, temp, volt, voltio
 	except CalledProcessError as exc :
-		return 1
+		retun ("E")
 
-def sdvolt(sdram):
-	pass
+def info(wlp):		# return available interfaces and IPs
+	for block in wlp.decode().split(": "):
+		if block.startswith('1'):
+			pass
+		elif block.startswith('lo'):
+			pass
+		elif block.startswith('<LO'):
+			pass
+		else :
+			blocks.append(block)
+	leng = int (len(blocks)/2)
+	for i in range(1,leng+1):
+		blocksip.append(blocks.pop(i))
+	for item in blocksip:
+		items = item.split(" ")
+		try:
+			point = items.index("inet")
+		except:
+			point = 0
+		if point == 0:
+			ips.append("Disconnected")
+		else : ips.append(items[point+1])
+	return blocks,ips
 
-def cpu():
-	pass
+try :
+	while True:
+		wlp, temp, volt, voltio = ifacecheck()
+		temp = temp.decode().strip("\n")
+		volt = volt.decode().strip("\n")
+		voltio = voltio.decode().strip("\n")
+		print (temp)
+		print (volt)
+		print (voltio)
+		ifs,ips = info(wlp)
+		count = len(ifs)
+		for n in range(count):
+			print ("> {}: is {}".format(ifs[n],ips[n]))
+		for n in range(count):
+			#print ("iface : {} ip :{}".format(ifs[n],ips[n+1]))
+			lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
+			lcd.lcd_string("{}:".format(ifs[n]), 1)
+			time.sleep(.1)
+			lcd.lcd_byte(lcd.LCD_LINE_2, lcd.LCD_CMD)
+			lcd.lcd_string("{}".format(ips[n]), 1)
+			time.sleep(3)
 
-def loopmode():
-	cpu = cputemp()
-	sdram = sdvolt("sdram_c")
-	sdrami = sdvolt("sdram_i")
-	sdramp = sdvolt("sdram_p")
-	myip = ip()
-
-	print ip
-
-#ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
-
-#while true == True:
-
-	#inin = input(">")
-
-	#lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
-	#lcd.lcd_string(inin, 2)
-	#time.sleep(1)
-#	lcd.GPIO.cleanup()
+		lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
+		lcd.lcd_string("cpu-{}".format(temp), 1)
+		time.sleep(.1)
+		lcd.lcd_byte(lcd.LCD_LINE_2, lcd.LCD_CMD)
+		lcd.lcd_string("@ {}".format(volt), 1)
+		time.sleep(1.5)
+		lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
+		lcd.lcd_string("I/O-{}".format(temp), 1)
+		time.sleep(.1)
+		lcd.lcd_byte(lcd.LCD_LINE_2, lcd.LCD_CMD)
+		lcd.lcd_string("@ {}".format(voltio), 1)
+		time.sleep(1.5)
+		ips = []
+		blocks = []
+		blocksip = []
+except :
+	exit(0)
